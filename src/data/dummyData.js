739bde9bd6b87c7109231021117ddf8923560de0
kasipulse <148783028+kasipulse@users.noreply.github.com>
2026-04-13 17:41:56 +0200

    Update dummyData.js

--- src/data/dummyData.js
+++ src/data/dummyData.js
@@ -1,1 +1,16 @@
-
+export const fuelData = [
+  {
+    id: 1,
+    vehicleId: "TRK-001",
+    authorizedAmount: 1000,
+    actualAmount: 1300,
+    litres: 60,
+  },
+  {
+    id: 2,
+    vehicleId: "TRK-002",
+    authorizedAmount: 800,
+    actualAmount: 780,
+    litres: 40,
+  },
+];
