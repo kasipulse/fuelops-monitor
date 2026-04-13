28f2f3715e3305231044f6831405a7f9be2b77ce
kasipulse <148783028+kasipulse@users.noreply.github.com>
2026-04-13 17:42:30 +0200

    Update analyzeFuel.js

--- src/utils/analyzeFuel.js
+++ src/utils/analyzeFuel.js
@@ -1,1 +1,9 @@
+export function analyzeFuel(item) {
+  const variance = item.actualAmount - item.authorizedAmount;
 
+  return {
+    ...item,
+    anomaly: Math.abs(variance) > 200,
+    variance,
+  };
+}
